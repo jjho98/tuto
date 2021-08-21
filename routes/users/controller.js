c623262429b10525f1aa188bd7a144ad9085044f
jjho98 <minorSunbi@gmail.com>
2021-08-21 19:38:38 +0900

    프로필 수정 api 작성

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -30,6 +30,7 @@ export const verifyNickname = async (nickname) => {
   return !!count;
 };
 
+// 내 프로필 정보 받아오기
 export const getMyInfo = async (req, res, next) => {
   try {
     const me = await user.findByPk(res.locals.user.id);
@@ -41,6 +42,35 @@ export const getMyInfo = async (req, res, next) => {
         'https://tuto-bucket.s3.ap-northeast-2.amazonaws.com/user-icon.png';
     }
     res.status(200).json(me);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// 내 프로필 정보 수정하기
+export const changeMyInfo = async (req, res, next) => {
+  try {
+    console.log(req.file);
+    console.log(req.body.thumbnail);
+
+    if (req.file) {
+      console.log('hit');
+    }
+
+    let { nickname, message, thumbnail } = req.body;
+    // 썸네일 파일 업로드가 이루어진 경우에만 req.file이 존재
+    if (req.file) {
+      thumbnail = req.file.location;
+    }
+    await user.update(
+      { nickname, message, thumbnail },
+      {
+        where: {
+          id: res.locals.user.id,
+        },
+      },
+    );
+    return res.status(204).json({});
   } catch (err) {
     next(err);
   }
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import * as userController from './controller';
+import upload from '../../lib/multer';
 
 const users = new Router();
 users.get('/me', userController.getMyInfo);
+users.put('/me', upload.single('thumbnail'), userController.changeMyInfo);
 
 export default users;
