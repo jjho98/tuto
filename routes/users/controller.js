3775283af8156b89ec105ad73950a9fb09cab9e0
jjho98 <minorSunbi@gmail.com>
2021-08-21 13:13:53 +0900

    비밀번호 삭제, 썸네일 없으면 기본값으로 전달

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -34,7 +34,12 @@ export const getMyInfo = async (req, res, next) => {
   try {
     const me = await user.findByPk(res.locals.user.id);
     // 비밀번호는 삭제 후 전달
-    delete me.password;
+    me.password = null;
+    // 썸네일이 null이면 기본 이미지 전달
+    if (!me.thumbnail) {
+      me.thumbnail =
+        'https://tuto-bucket.s3.ap-northeast-2.amazonaws.com/user-icon.png';
+    }
     res.status(200).json(me);
   } catch (err) {
     next(err);
