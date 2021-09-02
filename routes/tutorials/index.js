436f0fa73d7e40b3ed80fc39e00786d7536a6fb0
jjho98 <minorSunbi@gmail.com>
2021-09-02 21:29:06 +0900

    카테고리별 튜토리얼 읽어오는 api 엔드포인트 수정

--- routes/tutorials/index.js
+++ routes/tutorials/index.js
@@ -7,7 +7,7 @@ const tutorials = new Router();
 // id로 해당 튜토리얼 정보 보기
 tutorials.get('/:id', tutorialsController.getDetail);
 // category에 해당하는 튜토리얼들 모아보기
-tutorials.get('/:category', tutorialsController.list);
+tutorials.get('/category/:category', tutorialsController.list);
 // 튜토리얼 생성
 tutorials.post('/', upload.single(), tutorialsController.create);
 
